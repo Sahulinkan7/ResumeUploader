b57f97356b23eb869c43e6a34a2131e319eeaab3
Linkan Sahu <sahulinkan7@gmail.com>
2023-07-27 10:35:08 +0530

    migration done

--- ResumeUploader/resume/models.py
+++ ResumeUploader/resume/models.py
@@ -1,3 +1,53 @@
 from django.db import models
-
+STATE_CHOICE=(
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhatisgarh','Chhatisgarh'),
+    ('Dadra-Nagar Haveli and Daman & Diu','Dadra-Nagar Haveli and Daman & Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Ladakh','Ladakh'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Puducherry','Puducherry'),
+    ('Punjab','Punjab'),
+    ('Rajashtan','Rajashtan'),
+    ('Sikkim','Sikkim'),
+    ('Tamilnadu','Tamilnadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('West Bengal','West Bengal'),
+)
 # Create your models here.
+class Resume(models.Model):
+    name=models.CharField(max_length=100)
+    dob=models.DateField(auto_now=False,auto_now_add=False)
+    gender=models.CharField(max_length=100)
+    locality=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    pin=models.PositiveIntegerField()
+    state=models.CharField(choices=STATE_CHOICE,max_length=50)
+    mobile=models.PositiveIntegerField()
+    email=models.EmailField()
+    job_city=models.CharField(max_length=100)
+    profile_image=models.ImageField(upload_to="profileimages",blank=True)
+    document_file=models.FileField(upload_to="documents",blank=True)    
